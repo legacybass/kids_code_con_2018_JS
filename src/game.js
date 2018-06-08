6583a0107b06f4bccd4203aaa64dbdafa26c92e7
Daniel Beus <legacybass.developer@outlook.com>
2018-06-08 12:45:31 -0600

    Merged feature/spriteMovement
    commit aded7a650c8c0eb82f81d8fe4e815033d2184366
    Author: Daniel Beus <legacybass.developer@outlook.com>
    Date:   Fri Jun 8 12:23:06 2018 -0600
    
        Finished player movement and refactored enemy creation
    
    commit 4040b6a5700d56e64b1c0882a7d5f212dc72a006
    Author: Daniel Beus <legacybass.developer@outlook.com>
    Date:   Fri Jun 8 10:20:23 2018 -0600
    
        Moved enemy to its own class and added more sounds
    
    commit cb087164d82689b7a1badc6f17b7dbce0887ed98
    Author: Daniel Beus <legacybass.developer@outlook.com>
    Date:   Thu Jun 7 16:55:22 2018 -0600
    
        All level1 sprites spawning, enemies moving.
        - Moved logic into scenes.
        - Added game assets
        - Moved from Parcel to Webpack
        - Enemies and ship spawn
        - Enemies move

--- src/game.js
+++ src/game.js
@@ -1,4 +1,35 @@
 import Phaser from 'phaser';
-import config from './config';
+import Preloader from './scenes/preloader';
+import Level1 from './scenes/level1';
+
+const config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {}
+	},
+	scene: [
+		Preloader,
+		Level1
+	],
+	create
+};
 
 const game = new Phaser.Game(config);
+
+function create() {
+	this.data.set('lives', 3);
+	this.data.set('level', 1);
+	this.data.set('score', 0);
+
+	const text = this.add.text(5, 5, '', { font: '64px Courier', fill: '#0000FF' });
+	text.setText([
+		`Level: ${this.data.get('level')}`,
+		`Lives: ${this.data.get('lives')}`,
+		`Score: ${this.data.get('score')}`
+	]);
+
+	this.scene.start('preloader');
+}
